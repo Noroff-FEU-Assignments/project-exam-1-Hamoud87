eb3b1796711566c8d53f733e1dcdb5b17d0c0329
Hamoud87 <73738739+Hamoud87@users.noreply.github.com>
2021-09-20 11:26:46 +0200

    update

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,5 @@
-const blogUrl = "https://balawi.one/wp-json/wp/v2/posts?per_page=8";
+const blogUrl = "https://balawi.one/wp-json/wp/v2/posts?per_page=4";
+
 const blogContainer = document.querySelector(".blog-container");
 const button = document.querySelector("#button");
 async function getBlogPosts() {
@@ -8,14 +9,31 @@ async function getBlogPosts() {
     console.log(getBlogResults);
     blogContainer.innerHTML = "";
     for (let i = 0; i < getBlogResults.length; i++) {
-      if (i === 3) {
-        break;
-      } else {
-        button.onclick = function () {
-          i === 7;
-        };
-      }
+      blogContainer.innerHTML += ` <div class="blog-posts-wrapper" >
+                                   <a href="detail.html?id=${getBlogResults[i].id}">
+                                      <img src="${getBlogResults[i].featured_media_src_url}" alt="">
+                                      <h2>${getBlogResults[i].title.rendered}</h2>
+                                     </a>
+                                     <p>Published ${getBlogResults[i].date}</P>¢
+                                   </div>`;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getBlogPosts();
+let numberToOffset = 2;
+button.onclick = function () {
+  const viewMoreUrl = "https://balawi.one/wp-json/wp/v2/posts?per_page=${blogUrl}&offset=${numberToOffset}";
+};
 
+async function viewPosts() {
+  try {
+    const blogResponse = await fetch(viewMoreUrl);
+    const getBlogResults = await blogResponse.json();
+    console.log(getBlogResults);
+    blogContainer.innerHTML = "";
+    for (let i = 0; i < getBlogResults.length; i++) {
       blogContainer.innerHTML += ` <div class="blog-posts-wrapper" >
                                     <a href="detail.html?id=${getBlogResults[i].id}">
                                       <img src="${getBlogResults[i].featured_media_src_url}" alt="">
@@ -28,12 +46,4 @@ async function getBlogPosts() {
     console.log(error);
   }
 }
-getBlogPosts();
-
-/*if (i === 3) {
-  break;
-} else {
-  button.onclick = function () {
-  i++;
-  };
-}*/
+viewPosts();
